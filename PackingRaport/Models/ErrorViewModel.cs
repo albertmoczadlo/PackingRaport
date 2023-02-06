c29d9853ff2c2d82a6326416376962a9c4d75909
Albert <albertmoczadlo@gmail.com>
2023-02-06 13:54:25 +0100

    Add project files.

---
+++ PackingRaport/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PackingRaport.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
