69607d8dbb941b89b94f564c18c62f8a7b109a7d
Albert <albertmoczadlo@gmail.com>
2023-02-09 11:36:16 +0100

    Created controller raport, view index, repository raport and interface raport repositories.

---
+++ PackingRaport.Infrastructure/Repositories/RaportRepositories.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PackingRaport.Domain.InterfaceRepository;
+using PackingRaport.Domain.Models;
+using PackingRaport.Persistance.Context;
+
+namespace PackingRaport.Infrastructure.InterfaceRepository
+{
+    public class RaportRepositories : IRaportRepositories
+    {
+        private readonly RaportDbContext _context;
+
+        public RaportRepositories(RaportDbContext context)
+        {
+            _context=context;
+        }
+
+        public async Task<IEnumerable<Raport>> GetAllRaports()
+        {
+            var raports = await _context.Raports
+                .Include(x => x.User)
+                .Include(p => p.Products)
+                .Include(c => c.Containers)
+                .ThenInclude(t => t.Tanks)
+                .ToListAsync();
+
+            return raports;
+        }
+    }
+}
---
+++ PackingRaport/Controllers/RaportController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PackingRaport.Domain.InterfaceRepository;
+using PackingRaport.Domain.Models;
+
+namespace PackingRaport.Controllers
+{
+    public class RaportController : Controller
+    {
+        private readonly IRaportRepositories _raportRepositories;
+
+        public RaportController(IRaportRepositories raportRepositories)
+        {
+                _raportRepositories = raportRepositories;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var list = await _raportRepositories.GetAllRaports();
+
+            return View(list);
+        }
+    }
+}
